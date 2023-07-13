f9232217102b049531a63ab9c6842eaa255a636c
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-07-13 16:11:35 +0200

    pls check

--- devide-ngo-/routes/auth.routes.js
+++ devide-ngo-/routes/auth.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET signup page */
 router.get("/signup", (req, res, next) => {
-  res.render("index");
+  res.render("../views/auth/signup.ejs");
 });
 
 /* post data to register user */
