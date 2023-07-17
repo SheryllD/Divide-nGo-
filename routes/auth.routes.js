eb6a14f5f6a77beb92467dc86d0d95b36a09a39a
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-07-17 16:08:58 +0200

    database added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const User = require('../models/User.model'); 
 
 /* GET signup page */
 router.get("/signup", (req, res, next) => {
@@ -8,9 +8,15 @@ router.get("/signup", (req, res, next) => {
 }); 
 
 /* post data to register user */
-router.post("/signup", (req, res, next) => {
-    res.render("index");
+router.post("/signup", async(req, res, next) => {
+//    res.render("index");
+let newUser = await User.create(req.body) 
+res.redirect("/auth/login")
   });
+// bcrypt 
+
+// we also need to create a route for the user to post their bill/expense 
+// within the group we should be also able to get the info of other users expenses
 
 /* GET Login page */
 router.get("/login", (req, res, next) => {
