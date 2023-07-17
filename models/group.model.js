b995edcfe33b24571a97d03fede671630fe2f9de
Hamed Jalili <itjalili82@gmail.com>
2023-07-17 11:46:55 +0200

    group.model update

--- models/group.model.js
+++ models/group.model.js
@@ -1,1 +1,11 @@
-// this is just a test again!!!!
+const {Schema, model} = require("mongoose")
+
+const groupSchema = new Schema ({
+    title: {
+        type: String,
+    },
+    
+});
+
+const group = model("group", groupSchema);
+module.exports = group;
