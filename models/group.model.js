bf4512e675bf1eaccbce7f51d2e13510de6367fc
Hamed Jalili <itjalili82@gmail.com>
2023-07-17 14:56:24 +0200

    group.route update

--- models/group.model.js
+++ models/group.model.js
@@ -4,8 +4,54 @@ const groupSchema = new Schema ({
     title: {
         type: String,
     },
+description: {
+    type: String,
+    required: false,
+    trim: true
+  },
+  members: [{
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }],
     
 });
 
 const group = model("group", groupSchema);
 module.exports = group;
+
+
+
+
+
+/*  test  */
+// const { Schema, model } = require("mongoose");
+
+// const groupSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     unique: false,
+//     trim: true
+//   },
+//   description: {
+//     type: String,
+//     required: false,
+//     trim: true
+//   },
+//   members: [{
+//     type: Schema.Types.ObjectId,
+//     ref: "User"
+//   }],
+//   createdAt: {
+//     type: Date,
+//     default: Date.now
+//   },
+//   updatedAt: {
+//     type: Date,
+//     default: Date.now
+//   }
+// });
+
+// const Group = model("Group", groupSchema);
+
+// module.exports = Group;
