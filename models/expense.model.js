1c2f3d465d009fe000ffdc9cbe4cb6cef04b98bc
Hamed Jalili <itjalili82@gmail.com>
2023-07-18 14:07:09 +0200

    expense.model update

--- models/expense.model.js
+++ models/expense.model.js
@@ -1,1 +1,37 @@
-// hellooooooo
+const {Schema, model} = require("mongoose")
+
+const expenseSchema = new Schema({
+    groupId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Group',
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 32,
+        trim: true
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    paidBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, { timestamps: true });
+
+
+const group = model("expense", expenseSchema);
+module.exports = expense;
